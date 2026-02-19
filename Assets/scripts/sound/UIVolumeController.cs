c8d83656504ae818daf1b30b5082ea1f112ab025
goji <100840760+gybyro@users.noreply.github.com>
2026-02-19 08:00:46 +0000

    savor this while you can

--- Assets/scripts/sound/UIVolumeController.cs
+++ Assets/scripts/sound/UIVolumeController.cs
@@ -9,50 +9,10 @@ public class UIVolumeController : MonoBehaviour
     [Header("Slice Settings")]
     public float startTime = 0.22f;
     public float duration = 0.015f;
-    // public float duration = 0.28f;
-
-
     
 
-  
-    // void Start()
-    // {
-    //     // StartCoroutine(PlayAmbient());
-    // }
 
-    // float t;
-
-    // void Update()
-    // {
-    //     // t += Time.deltaTime * 0.2f;
-    //     // source.pitch = Mathf.Lerp(0.7f, 1.4f, Mathf.PerlinNoise(t, 0));
-    // }
-
-    // IEnumerator PlayAmbient()
-    // {
-    //     while(true)
-    //     {
-    //         source.clip = SliceClip(originalClip, 0.22f, 0.28f);
-    //         source.Play();
-    //         // PlaySlice(0.22f, 0.28f);
-    //         // source.pitch = 0.5f;
-
-    //         // source.pitch = notes[Random.Range(0, notes.Length)];
-    //         // source.volume = Random.Range(0.15f, 0.35f);
-
-    //         // source.PlayOneShot(source.clip);
-
-    //         // yield return new WaitForSeconds(
-    //         //     Random.Range(0.6f, 2.5f)
-    //         // );
-
-
-    //         // source.spatialBlend = 0f; // 2D sound
-    //         // source.loop = false;
-    //     }
-    // }
-
-    public void PlayWhistle()
+    public void PlayUIWhistle()
     {
         PlaySlice(startTime, duration);
     }
@@ -74,50 +34,4 @@ public class UIVolumeController : MonoBehaviour
         source.Stop();
     }
 
-    // public void PlaySlice(float start, float end)
-    // {
-    //     AudioClip clip = source.clip;
-
-    //     int startSample = (int)(start * clip.frequency);
-    //     int lengthSamples = (int)((end - start) * clip.frequency);
-
-    //     source.timeSamples = startSample;
-    //     source.Play();
-
-    //     float duration = (float)lengthSamples / clip.frequency;
-    //     Invoke(nameof(StopSound), duration);
-    // }
-
-    // void StopSound()
-    // {
-    //     source.Stop();
-    // }
-
-    // // ============= subclip
-    // AudioClip SliceClip(AudioClip clip, float start, float end)
-    // {
-    //     start = Mathf.Clamp(start, 0f, clip.length);
-    //     end   = Mathf.Clamp(end, start, clip.length);
-
-    //     int frequency = clip.frequency;
-    //     int channels = clip.channels;
-
-    //     int startSample = Mathf.FloorToInt(start * frequency);
-    //     int sampleLength = Mathf.FloorToInt((end - start) * frequency);
-
-    //     float[] data = new float[sampleLength * channels];
-
-    //     clip.GetData(data, startSample);
-
-    //     AudioClip newClip = AudioClip.Create(
-    //         clip.name + "_slice",
-    //         sampleLength,
-    //         channels,
-    //         frequency,
-    //         false
-    //     );
-
-    //     newClip.SetData(data, 0);
-    //     return newClip;
-    // }
 }
